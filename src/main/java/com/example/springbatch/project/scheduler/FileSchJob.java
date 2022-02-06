54f455469d26f024c952279639df16fe5c04622d
prodo-developer <wjdals300@gmail.com>
2022-02-06 17:22:30 +0900

    refactor: 파일 중복등록을 막기위한 JobExplorer 적용 및 분기처리

--- src/main/java/com/example/springbatch/project/scheduler/FileSchJob.java
+++ src/main/java/com/example/springbatch/project/scheduler/FileSchJob.java
@@ -3,15 +3,16 @@ package com.example.springbatch.project.scheduler;
 import lombok.SneakyThrows;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class FileSchJob extends QuartzJobBean {
@@ -21,6 +22,9 @@ public class FileSchJob extends QuartzJobBean {
 
     @Autowired
     private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobExplorer jobExplorer; // jobRepository의 read 기능만 가지고 있음
 
     /**
      * 배치를 실행시키는 구문
@@ -37,6 +41,28 @@ public class FileSchJob extends QuartzJobBean {
                 .addLong("id", new Date().getTime())
                 .addString("requestDate", requestDate)
                 .toJobParameters();
+
+        // 모든 job의 인스턴스 갯수를 가져올수 있음.
+        int jobInstanceCount = jobExplorer.getJobInstanceCount(fileJob.getName());
+
+        // 모든 job의 인스턴스 정보를 가져올수 있음. 0번째 부터 카운트 갯수까지
+        List<JobInstance> jobInstances = jobExplorer.getJobInstances(fileJob.getName(), 0, jobInstanceCount);
+
+        if(jobInstances.size() > 0) {
+            for (JobInstance jobInstance : jobInstances) {
+                // 여러개의 jobExecution을 가져온다.
+                List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+                List<JobExecution> jobExecutionList = jobExecutions.stream()
+                        .filter(jobExecution -> jobExecution.getJobParameters().getString("requestDate").equals(requestDate))
+                        .collect(Collectors.toList());
+
+                // 해당하는 날짜가 1개 이상인경우 배치를 실행하지 않는다.
+                if(jobExecutionList.size() > 0) {
+                    throw new JobExecutionException(requestDate + " already exists");
+                }
+            }
+
+        }
 
         jobLauncher.run(fileJob, jobParameters);
     }
