edc53d86258ade1c9faec81c8b030712e4567c5d
prodo-developer <wjdals300@gmail.com>
2022-02-06 17:21:13 +0900

    refactor: ApiResponseVO를 통해 가져오는 item정보를 저장하는 경로 설정추가

--- src/main/java/com/example/springbatch/project/batch/chunk/writer/ApiItemWriter1.java
+++ src/main/java/com/example/springbatch/project/batch/chunk/writer/ApiItemWriter1.java
@@ -3,11 +3,14 @@ package com.example.springbatch.project.batch.chunk.writer;
 import com.example.springbatch.project.batch.domain.ApiRequestVO;
 import com.example.springbatch.project.batch.domain.ApiResponseVO;
 import com.example.springbatch.project.service.AbstractApiService;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.core.io.FileSystemResource;
 
 import java.util.List;
 
-public class ApiItemWriter1 implements ItemWriter<ApiRequestVO> {
+public class ApiItemWriter1 extends FlatFileItemWriter<ApiRequestVO> {
 
     private final AbstractApiService apiService;
 
@@ -19,5 +22,14 @@ public class ApiItemWriter1 implements ItemWriter<ApiRequestVO> {
     public void write(List<? extends ApiRequestVO> items) throws Exception {
         ApiResponseVO responseVO = apiService.service(items);
         System.out.println("responseVO = " + responseVO);
+
+        // item마다 저장
+        items.forEach(item -> item.setApiResponseVO(responseVO));
+
+        super.setResource(new FileSystemResource("C:\\Programer\\spring-study\\spring-batch\\src\\main\\resources\\product1.txt"));
+        super.open(new ExecutionContext());
+        super.setLineAggregator(new DelimitedLineAggregator<>()); // 구분자방식
+        super.setAppendAllowed(true); // 계속 추가하는 기능
+        super.write(items); // 부모에게 items를 넘거줌
     }
 }
